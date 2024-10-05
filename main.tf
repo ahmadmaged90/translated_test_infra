69c8800ae6873542d903902084c24ae93341e48f
ahmad.elgrib <ahmad.elgrib@accenture.com>
2024-10-05 16:07:40 +0200

    add docker file

--- main.tf
+++ main.tf
@@ -390,11 +390,11 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
             essential = true
             logconfiguration = {
                 logDrive = "awslogs"
-                 options = {
+                options = {
                     "awslogs-group" = aws_cloudwatch_log_group.ecs_log_group.name
                     "awslogs-region"       = var.region
                     "awslogs-stream-prefix" = "ecs"
-                 }                
+                }                
             }
             portMappings = [
                 {
